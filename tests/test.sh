be7c615de63f6687f2aa32d8bb1e9d33db1174bf
suwoom <23qkamohalalalajun@gmail.com>
2023-09-09 13:30:15 +0900

    テストスクリプトを書き換え

--- tests/test.sh
+++ tests/test.sh
@@ -1,15 +1,36 @@
-#!/bin/bash
-if [ "$1" != "--no-build" ]; then
+function usage {
+cat >&2 <<EOS
+コマンドの説明
+[options]
+ -h | --help:      ヘルプを表示
+ --no-build:       コンパイラをビルドしない
+ --apple-silicon:  apple-silicon mac
+ -d | --docker-image <VALUE>:  実行するdockerのイメージ名
+EOS
+exit 1
+}
+
+IMAGE=compilerbook
+while [ "$#" != 0 ]; do
+  case $1 in
+    -h | --help      ) usage;;
+    --no-build       ) NO_BUILD=true;;
+    --apple-silicon  ) APPLE_SILICON=true;;
+    -d | --docker-image ) shift; IMAGE=$1;;
+    -* | --*         ) echo "$1 : invalid option" ;;
+  esac
+  shift
+done
+
+if ! ${NO_BUILD:-false}; then
   cargo build
 fi
+
 ./target/debug/rnc tests/tests.c > bind/tmp.s
-# CMD='echo "int char_fn() { return 257; } int static_fn() { return 5; } int ext1; int *ext2; int ext3 = 5;"'
-# CMD+=' |  gcc -xc -c -o tmp2.o -;'
-CMD+="gcc -static -o tmp tmp.s && ./tmp"
+CMD="gcc -static -o tmp tmp.s && ./tmp"
 
-# if intel
-# docker run --rm --mount type=bind,src=$PWD/bind,dst=/home/user --workdir /home/user compilerbook /bin/bash -c "$CMD"
-
-# if apple silicon
-# ubuntuのイメージをpullして、gccを入れたやつをubuntu2とした。
-docker run --rm --platform linux/amd64 --mount type=bind,src=$PWD/bind,dst=/home/user --workdir /home/user ubuntu2 /bin/bash -c "$CMD"
+if ${APPLE_SILICON:-false}; then
+  docker run --rm --platform linux/amd64 --mount type=bind,src=$PWD/bind,dst=/home/user --workdir /home/user ${IMAGE} /bin/bash -c "$CMD"
+else
+  docker run --rm --mount type=bind,src=$PWD/bind,dst=/home/user --workdir /home/user ${IMAGE} /bin/bash -c "$CMD"
+fi
